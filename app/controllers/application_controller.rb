750abf681c09d65ff5413fb9a5aa7ef0cc9e435b
Brian Harvey <brian.harv3y@gmail.com>
2020-11-02 11:05:29 -0800

    app now saves user's Name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit(:email, :password, :password_confirmation, :avatar, :name)
     end
+
+    devise_parameter_sanitizer.permit(:sign_up,  keys: [:name])
   end
 
 end
