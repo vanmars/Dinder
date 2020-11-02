169abca17e9702c34d1c2ad9d6a29c43c16722b9
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-02 10:24:54 -0800

    Add functionality to search for friends and add them if they are not already friends.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
   
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+    if params[:add_friend]
+      current_user.friends << @user
+      redirect_to user_path(current_user)
+    end
     render :show
+
   end
 
   def friends
+    @user = User.find(params[:id])
+    if params[:friend_search]
+      @friends = User.friend_search(params[:friend_search])
+    end
     render :friends
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   has_and_belongs_to_many :friends,
                           class_name: "User",
                           join_table: :friendships,
                           foreign_key: :user_id,
                           association_foreign_key: :friend_user_id
+
+
+  scope :friend_search, -> (friend_parameter) { where("name ilike ?", "%#{friend_parameter}%") }
+
 end
