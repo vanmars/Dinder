fdab8f10be6c22197a26592dcbce9b7f241490ab
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-03 14:10:52 -0800

    Add functionality for searching any city

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,7 +8,7 @@ class RestaurantsController < ApplicationController
     @restaurants = restaurant.get_restaurants_trending(city_id)
     if params[:city_search] && params[:name_search]
       restaurant2 = Restaurant.new(params[:city_search])
-      city_id2 = restaurant.get_city_id
+      city_id2 = restaurant2.get_city_id
       @restaurants2 = restaurant2.search(city_id2, params[:name_search])
     end
     render :explore
